97d4c121dfab567c45d7816d23f650f8092a844f
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2021-08-22 19:06:47 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,47 +1,12 @@
-var express = require('express');
-var app = express();
 var express = require("express");
 var app = express();
 
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
